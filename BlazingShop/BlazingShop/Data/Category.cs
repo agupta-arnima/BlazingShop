31d1c57cc008ce5ae1be6d3190d620b6d99a6f47
Anand Prakash Gupta <agupta.arnima2@gmail.com>
2020-06-20 19:13:15 +0530

    Initial Commit to Blazor Shop

---
+++ BlazingShop/BlazingShop/Data/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Data
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; } //Entity Framework will identify this as Identity and Primary Key Column
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
---
+++ BlazingShop/BlazingShop/Pages/Learn Blazor/LearnBlazor.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingShop.Pages
+{
+    public class LearnBlazorBase : ComponentBase
+    {
+        protected string name = null;
+        protected string WelcomeText = "Time to learn blazor!";
+
+        protected void getName()
+        {
+            name = "Blazor Learner";
+        }
+    }
+}
